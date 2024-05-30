11d6369ad97f3f5d6c73052395d98b9334da79b9
CodeYouness <ylijassi@gmail.com>
2024-05-30 22:16:26 +0200

    added style to css adding dragable element

--- js/script.js
+++ js/script.js
@@ -4,16 +4,24 @@ createApp({
     data() {
         return {
             todolist: [{
-                'content': 'patate',
-                'done': false
+                'content': 'Comprare bici',
+                'done': false,
+                'id': 0
             },
             {
-                'content': 'carote',
-                'done': true
+                'content': 'Finire corso Boolean',
+                'done': false,
+                'id': 1
             },
             {
-                'content': 'cipolle',
-                'done': false
+                'content': 'Riparare orologio',
+                'done': false,
+                'id': 2
+            },
+            {
+                'content': 'Andare in palestra',
+                'done': false,
+                'id': 3
             }],
             insertText: ''
         }
@@ -31,17 +39,33 @@ createApp({
         addTask: function (newContent) {
             const newTask = {
                 'content': '',
-                'done': false
+                'done': false,
+                'id': this.todolist.length
             }
-            if (newContent.length >= 1)
+            if (newContent.length > 1) {
                 newTask.content = newContent
-            this.todolist.push(newTask)
-            this.insertText = ''
+                this.todolist.push(newTask)
+                this.insertText = ''
+            }
         },
 
         overWriteTask: function (index) {
             const newTask = prompt('overwrite the task: ' + this.todolist[index].content)
-            this.todolist[index].content = newTask
-        }
+            if (newTask.length > 1) {
+                this.todolist[index].content = newTask
+
+            }
+        },
+        startDrag(evt, item) {
+            evt.dataTransfer.dropEffect = 'move'
+            evt.dataTransfer.effectAllowed = 'move'
+            evt.dataTransfer.setData('itemID', item.id)
+        },
+
+        onDrop(evt, list) {
+            const itemID = evt.dataTransfer.getData('itemID')
+            const item = this.todolist.find((item) => item.id == itemID)
+            item.list = list
+        },
     }
 }).mount('#app')
